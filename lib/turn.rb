c983410e85c62e70a968ea304175aad0d26d1382
Liam Roberts <liam_r_123@hotmail.co.uk>
2018-12-17 14:42:59 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,7 +7,7 @@ def display_board(board)
 end
 
 def input_to_index(input)
-  input.to_i - 1
+  index = input.to_i - 1
 end
 
 def valid_move?(board, index)
@@ -25,7 +25,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  index = input_to_index(input)
+  input_to_index(input)
   if valid_move?(board, index)
     move(board, index)
     display_board(board)
